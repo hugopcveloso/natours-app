998ac59d7a3ebef4067c75771301ae50388006b0
Hugo V <hugopcveloso@gmail.com>
2021-02-25 18:04:35 +0000

    fixing checkout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -63,7 +63,7 @@ const createBookingCheckout = catchAsync(async (session) => {
   await Booking.create({ tour, user, price });
 });
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
@@ -77,7 +77,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
   if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+    await createBookingCheckout(event.data.object);
   res.status(200).json({ received: true });
 };
 
