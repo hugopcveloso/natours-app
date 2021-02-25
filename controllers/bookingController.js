03aecdfe6db021108d93a35d94635c67506eaa81
Hugo V <hugopcveloso@gmail.com>
2021-02-25 18:40:13 +0000

    fixing issues with stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,12 +59,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]); //taking the query string out
 // });
 
-const createBookingCheckout = catchAsync(async (session) => {
+const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
-});
+};
 
 exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
