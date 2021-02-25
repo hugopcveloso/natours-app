d58dae4ede4c5c2017d9cd7580aa63d7a634156b
Hugo V <hugopcveloso@gmail.com>
2021-02-25 18:17:46 +0000

    stripe issue

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,7 +57,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]); //taking the query string out
 // });
 
-exports.createBookingCheckout = catchAsync(async (session) => {
+const createBookingCheckout = catchAsync(async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
@@ -68,7 +68,7 @@ exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    const event = stripe.webhooks.constructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
